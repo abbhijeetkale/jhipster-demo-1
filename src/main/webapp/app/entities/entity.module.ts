807e63c42e92742c01507fee3d69139f9c64c366
Abhijee Kale <akale@netradius.com>
2018-11-11 14:51:40 +0530

    Implementing the blog

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,10 +1,16 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { Jhipsterdemo1BlogModule } from './blog/blog.module';
+import { Jhipsterdemo1EntryModule } from './entry/entry.module';
+import { Jhipsterdemo1TagModule } from './tag/tag.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     // prettier-ignore
     imports: [
+        Jhipsterdemo1BlogModule,
+        Jhipsterdemo1EntryModule,
+        Jhipsterdemo1TagModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
